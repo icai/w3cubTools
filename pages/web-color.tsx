630746a88fe147fbe9ca663f9cc3ee198daf83d1
Terry Cai <gidcai@gmail.com>
2019-07-20 13:03:43 +0800

    update web color

--- pages/web-color.tsx
+++ pages/web-color.tsx
@@ -32,12 +32,6 @@ export default class extends Component<any, any> {
     const { colors } = this.state;
     return (
       <Fragment>
-        <Head>
-          <link
-            rel="stylesheet"
-            href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          />
-        </Head>
         <div className="ColorCentWrap pt10">
           <ul className="SafeCorCent-down" style={{ border: 0 }}>
             {colors.map(item => {
