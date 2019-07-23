9d62b89b2ad0267710891a71b3385a63b2418275
Terry Cai <gidcai@gmail.com>
2019-07-23 17:09:15 +0800

    add exmaple button

--- pages/base64-to-image.tsx
+++ pages/base64-to-image.tsx
@@ -2,6 +2,9 @@ import ConversionLayout from "@components/ConversionLayout";
 import * as React from "react";
 import { Button } from "evergreen-ui";
 
+const exmaple =
+  "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAIAAABuYg/PAAACgUlEQVR4nOyWvUvzXhTHb5pEiIkYFHRRCAhu1RjRiG+Db4iujoKL4OLg6j/g6NylYzsH+ke0paVpS+jSNl3aUkrp+1JIkx/8ApfjTZOHLg7P0zud870n53PuvSc3YRzHQb81Qr9GWsAWsH8XxkDn+/u72WxC5f39XZIk7Eaj0XK57N4DjuNQFPX19UXTtDtr2/bn5+d0OsXxW1tbHx8fs2GTySSRSEBlb28PwiKRyHA4hAHPz8/hcNi1TdPUNA3Ovr6+QvfHNt7c3BALLxaL2DZNczAYEAH5fB7buq4Ts0TCHzBFUdbX16GSy+VgLoqiAmCwMoTQxsbG/v6+L4ym6aurK6g0Go12u+3NOxMGK3OXFQqFfGEIodvbW0JJJpMBsHq93mq1EEKj0aharcKpu7s7IpiEnZ6e8jwPFfckxuNxpVLxwvDuZbNZ+LVaXV09Pj7+A4xlWaKiVCrlZrRt21V4nhcEAQe4u0d0x8PDA34lfGFuHHRrtVqv14N7GP5/EEtPp9PwqcfHR2/mGbCzszNRFKGSyWTg4auqenR0hF3DMPr9vmEYWNnc3IQBQTCapon3Q9f1QqGAXVmWYU9blhWPxy3Lwoq3y3xh3mhN0/DFwTDMwcGBLMuwrWOxGIz39mEQTFXV5eVl7Ha7XWzv7u5yHCcIws7ODhY7nQ62RVFUFGUOGMdxl5eXfnW4xsxTQQhdX1+zLDsHDCF0f38/U8dVy7I814NBsPPzc2+BjuMcHh4SVDh4nj85OZkbtrKygncMD0mS8E29vb29trZGBFxcXCwtLfnlZPwmEEIvLy/ERwAvyx1vb2+lUgkqT09PAQmpxe/3AraA/V2w/wIAAP//kdDhyy4UpR4AAAAASUVORK5CYII=";
+
 export default function() {
   return (
     <ConversionLayout
@@ -28,18 +31,6 @@ export default function() {
               Generate Image
             </Button>
             <Button
-              is="a"
-              marginRight={10}
-              height={40}
-              margin="5px"
-              display="block"
-              whiteSpace="nowrap"
-              download="image.jpg"
-              href={result}
-            >
-              Download Image
-            </Button>
-            <Button
               marginRight={10}
               height={40}
               margin="5px"
@@ -53,6 +44,36 @@ export default function() {
               }}
             >
               Empty All
+            </Button>
+            <Button
+              marginRight={10}
+              height={40}
+              margin="5px"
+              display="block"
+              whiteSpace="nowrap"
+              onClick={() => {
+                var str = exmaple.trim();
+                setValue(str);
+                // data:image/png;base64
+                if (str.substring(0, 4) != "data") {
+                  str = "data:image/png;base64," + str;
+                }
+                setResult(str);
+              }}
+            >
+              Get Example
+            </Button>
+            <Button
+              is="a"
+              marginRight={10}
+              height={40}
+              margin="5px"
+              display="block"
+              whiteSpace="nowrap"
+              download="image.jpg"
+              href={result}
+            >
+              Download Image
             </Button>
           </>
         );
